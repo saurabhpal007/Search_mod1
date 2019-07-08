cf1b13d7651ae7bef519a7f44716fb9d97864b60
Saurabh Pal <palsaurabh63@gmail.com>
2019-07-09 00:17:39 +0530

    Add files via upload

---
+++ getUs.php
@@ -0,0 +1,44 @@
+<?php
+
+$conn=mysqli_connect("localhost","root","","test3");
+
+if(isset($_GET['key'])){
+	$key=$_GET['key'];
+	
+
+	$query="SELECT * FROM `lo` WHERE MATCH(name) Against('$key')"; //will use boolean mode after sucessful deployment to app
+	
+	$result=mysqli_query($conn,$query);	
+	
+		$response =array();
+		while($row=mysqli_fetch_assoc($result)){
+		array_push($response,
+		array(
+			'id'=>$row['id'],
+			'name'=>$row['name'])
+			);			
+			}		
+		
+			
+	echo json_encode($response);
+}
+//if key remains empty all data printed just for testing
+else{
+	$query="SELECT * FROM lo";
+	$result=mysqli_query($conn,$query);
+	$response =array();
+		while($row=mysqli_fetch_assoc($result)){
+		array_push($response,
+		array(
+			'id'=>$row['id'],
+			'name'=>$row['name'])
+			);
+	
+	
+}
+echo json_encode($response);
+}
+
+mysqli_close($conn);
+
+?>
